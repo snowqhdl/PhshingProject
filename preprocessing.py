b461028d6f46cf0e87ce96ce65fea6541bf99da5
s5555s <91522318+s5555s@users.noreply.github.com>
2023-08-17 18:43:29 +0900

    추가된 요소 갱신
    2.1. 사이트 내 외부 URL 비율
    2.6.1. 웹사이트에서 mailto
    2.6.2. 웹사이트에서 mail 
    3.3. 오른쪽 클릭 비활성화
    4.1. 도메인 수명

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,8 @@
 from urllib import parse
 from tld import get_tld
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import whois
 import requests
 import ssl
 import socket
@@ -126,7 +129,7 @@ def ssl_fs_dec(url: str):
         return -1
     except Exception as e:
         return -1
-'''    
+'''
 ## 1.9. 도메인 유효 기간을 통한 판단 기준
 def is_phishing_site(url):
     try:
@@ -153,8 +156,7 @@ def is_phishing_site(url):
     except:
         # 예외 발생 시 (예: 도메인 정보를 찾을 수 없는 경우) 정상 사이트로 판별
         return 1
-'''
-'''
+
 ## 1.10. favicon을 통한 판단 기준
 def check_favicon(url):
     try:
@@ -180,14 +182,13 @@ def check_favicon(url):
                 return -1
 
         return 1
+'''
 
     except Exception as e:
         print("An error occurred:", str(e))
         return 0  # 에러 발생 시 0 반환
 
-'''
-
-## 1.11. 비표준 포트 사용
+## 1.11. 비표준 포트 사용 여부를 통한 판단 기준
 ##       HTTPS 표준 포트 연결 가능 여부 판단
 ##       HTTPS 표준 포트 연결 불가일 경우 예비 포트로 진입
 ## 정상 : 1 / 의심 : 0 / 피싱 : -1
@@ -208,5 +209,150 @@ def non_std_port(url: str):
         except (socket.timeout, ConnectionRefusedError, socket.gaierror):
               return -1
 
+## 2.1. 사이트 내 외부 URL 비율을 통한 판단 기준
+def reque_dec(url):
+    try:
+        # WebDriver 인스턴스를 생성합니다.
+        # 이 예제에서는 Chrome 드라이버를 사용합니다.
+        driver = webdriver.Chrome(executable_path="path_to_chromedriver")
 
+        # URL의 웹페이지를 로드합니다.
+        driver.get(url)
 
+        # 동적으로 로드된 페이지의 소스를 가져옵니다.
+        page_source = driver.page_source
+
+        # WebDriver 인스턴스를 종료합니다.
+        driver.quit()
+
+        soup = BeautifulSoup(page_source, "html.parser")
+        base_domain = urlparse(url).netloc
+        external_objects_count = 0
+        total_objects_count = 0
+
+        for tag in soup.find_all(['img', 'video', 'audio']):
+            src = tag.get('src')
+            if src:
+                total_objects_count += 1
+                if base_domain not in urljoin(url, src):
+                    external_objects_count += 1
+
+        if total_objects_count == 0:
+            return -1
+        
+        ratio = (external_objects_count / total_objects_count) * 100
+
+        if ratio < 22:
+            return 1
+        elif ratio < 61:
+            return 0
+        else:
+            return -1
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return 0
+
+## 2.6.1. 웹사이트에서 mailto 허용 여부를 통한 판단 기준
+def mailto_dec(url: str):
+    try:
+        ## requests를 이용한 웹사이트 응답 요청
+        response = requests.get(url)
+        response.raise_for_status()
+
+        ## mailto: 링크가 포함되어 있는지 확인
+        if 'mailto:' in response.text:
+            return 0
+        else:
+            return 1
+    ## 요청에서 오류 발생
+    except requests.RequestException as e:
+        print(f"Error fetching the website: {e}")
+        return -1
+
+## 2.6.2. 웹사이트에서 mail 사용 여부를 통한 판단 기준
+def mail_dec(url: str):
+    try:
+        ## 
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        if "mail(" in response.text:
+            return -1  # mail() 사용
+        else:
+            return 1  # mail() 사용하지 않음
+
+    except requests.RequestException as e:
+        print(f"Error fetching the website: {e}")
+        return 0  # 서버 접근 시도 시 막힘
+
+## 3.3. 오른쪽 클릭 비활성화를 통한 판단 기준
+def right_dec(url):
+    # 먼저 BeautifulSoup으로 시도합니다.
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        
+        if soup.find(lambda tag: tag.name.lower() in ["body", "html"] and 
+                      tag.has_attr("oncontextmenu") and 
+                      "return false" in tag.attrs["oncontextmenu"].lower()):
+            return -1
+
+        scripts = soup.find_all("script")
+        for script in scripts:
+            if script.string and "event.button == 2" in script.string:
+                return -1
+    except Exception as e:
+        print(f"Error with BeautifulSoup: {e}")
+
+    # BeautifulSoup으로 우클릭 방지 코드를 찾지 못했다면 Selenium으로 시도합니다.
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu") 
+    options.add_argument("--no-sandbox")
+
+    try:
+        with webdriver.Chrome(executable_path="YOUR_CHROMEDRIVER_PATH", options=options) as driver:
+            driver.get(url)
+
+            body_elements = driver.find_elements_by_xpath("//body[@oncontextmenu='return false']") + \
+                            driver.find_elements_by_xpath("//html[@oncontextmenu='return false']")
+            if body_elements:
+                return -1
+
+            scripts = driver.find_elements_by_tag_name("script")
+            for script in scripts:
+                if script.get_attribute('innerHTML') and "event.button == 2" in script.get_attribute('innerHTML'):
+                    return -1
+
+            return 1
+    except Exception as e:
+        print(f"Error with Selenium: {e}")
+        return 0
+
+## 4.1. 도메인 수명을 통한 판단 기준
+##      피싱사이트 수명이 짧은 것을 이용
+
+def domain_age_check(url: str):
+    try:
+        w = whois.whois(url)
+
+        ## 도메인의 생성 날짜 또는 만료 날짜가 없는 경우
+        if w.creation_date is None or w.expiration_date is None:
+            return 0
+        elif isinstance(w.creation_date, list):
+            creation_date = w.creation_date[0]
+        else:
+            creation_date = w.creation_date
+            
+        current_date = datetime.datetime.now()
+        domain_age = (current_date - creation_date).days
+        
+        if domain_age < 6 * 30:  # 대략 6개월을 날짜로 계산
+            return -1
+        else:
+            return 1  # 정상 사이트
+
+    ## 예외 발생하는 경우 whois를 지원하지 않는 경우
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return 0
